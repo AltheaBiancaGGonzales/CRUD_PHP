92403a49f5711490f3018dd9eb8271d34725c8e9
Althea Gonzales <altheagonzalesctr@gmail.com>
2025-05-23 06:10:47 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$db   = 'pets_information';
+$user = 'f07f570e7afc80002d5b27548832';
+$pass = 'your_password';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     die("Database connection failed: " . $e->getMessage());
+}
+?>
